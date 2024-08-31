45f8e0e0d51544cafe54dc063ec82311a8fcc906
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2024-08-30 21:23:25 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,29 @@ import { useState, useEffect } from 'react'
 import io from 'socket.io-client';
 import './App.css'
 
-const socket = io('ws://10.3.32.27:3000', {
+const socket = io('ws://10.3.58.27:3000', {
   transports: ['websocket']
 });
 
-function App() {  
+type message = {
+  client: string,
+  text: string
+}
 
-  const [messages, setMessages] = useState<string[]>([]);
+function App() {  
+  const [userId, setUserId] = useState<string>('')
+  const [messages, setMessages] = useState<message[]>([]);
   
   const [message, setMessage] = useState<string>('');
 
   useEffect(() => {
     
-    socket.on('MESSAGE', (newMessage: string) => {
+    socket.on('MESSAGE', (newMessage: message) => {
       setMessages((prevMessages) => [...prevMessages, newMessage]);
+      console.log(newMessage)
     });
+
+    setUserId(socket.id || '')
 
     return () => {
       socket.off('MESSAGE');
@@ -26,19 +34,35 @@ function App() {
   const sendMessage = () => {
     if (message.trim()) {
       socket.emit('MESSAGE', message);
+      setUserId(socket.id || '')
       setMessage('');
     }
   };
 
   return (
     <>
-      <div>
+      <div className='largo'>
       <h1>Chat</h1>
       <div>
-        <div>
-          {messages.map((msg, index) => (
-            <div key={index}>{msg}</div>
-          ))}
+        <div className='mensagens'>
+          {messages.map((msg: message, index) => {
+            console.log(msg)
+            console.log("user: " + userId)
+            console.log("msg: " + msg.client)
+            if(userId == msg.client){
+            return (                        
+              <div className='mensagem' key={index}>
+                <div>
+                  {msg.text}
+                </div>
+                </div>
+            )
+            }else{
+              return (                        
+                <div className='mensagem outrouser' style={{justifyContent: 'flex-start'}} key={index}><div>{msg.text}</div></div>
+              )
+            }
+          })}
         </div>
         <input
           type="text"
