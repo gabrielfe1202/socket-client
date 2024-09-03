8b7eab453fe80e571f6e9a34e041a20502eedad7
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2024-09-02 21:42:07 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import io from "socket.io-client";
 import Chat from './pages/Chat';
+
+const socket = io("ws://10.3.41.20:3000", {
+  transports: ["websocket"],
+});
 
 function Home() {
   const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
     if(name != ""){
-      navigate(`/Chat?name=${encodeURIComponent(name)}`);
+      navigate(`/Chat?name=${encodeURIComponent(name)}&room=${encodeURIComponent(room)}`);
     }else{
       alert("Nome invalido!")
     }
   }
-  ;
   function capitalizeFirstLetter(string: string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
   }
@@ -24,6 +29,19 @@ function Home() {
     <div>
       <h1 style={{textAlign: 'center'}}>Diga seu nome</h1>
       <form onSubmit={handleSubmit} style={{display: 'flex',flexDirection: 'column'}}>
+      <input
+        style={{          
+          paddingInline: 15,
+          paddingTop: 5,
+          paddingBottom: 5,
+          fontSize: 20,
+          marginBottom: 15
+        }}
+          type="text"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          placeholder="Sala"
+        />
         <input
         style={{          
           paddingInline: 15,
@@ -37,8 +55,9 @@ function Home() {
           onChange={(e) => setName(capitalizeFirstLetter(e.target.value))}
           placeholder="Nome"
         />
-        <button type="submit">Entrar</button>
+        <button type="submit">Entrar</button>        
       </form>
+      <button style={{backgroundColor: 'rgba(0,0,0,.0)', border: 'none' }}>Criar sala</button>
     </div>
   );
 }
--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,105 +1,121 @@
-import { useState, useEffect, useRef } from 'react'
-import io from 'socket.io-client';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import '../App.css'
+import { useState, useEffect, useRef } from "react";
+import io from "socket.io-client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import "../App.css";
 
-const socket = io('ws://192.168.15.18:3000', {
-    transports: ['websocket']
+const socket = io("ws://10.3.41.20:3000", {
+  transports: ["websocket"],
 });
 
 type message = {
-    client: string,
-    text: string,
-    name: string,
-}
+  client: string;
+  text: string;
+  name: string;
+  type: string;
+};
 
 function Chat() {
-    const [userId, setUserId] = useState<string>('')
-    const [messages, setMessages] = useState<message[]>([]);
-    const [message, setMessage] = useState<string>('');
-    const chatContainerRef = useRef(null);
+  const [userId, setUserId] = useState<string>("");
+  const [messages, setMessages] = useState<message[]>([]);
+  const [message, setMessage] = useState<string>("");
+  const chatContainerRef = useRef(null);
 
-    const queryParams = new URLSearchParams(window.location.search);
-    const name = queryParams.get("name");
+  const queryParams = new URLSearchParams(window.location.search);
+  const name = queryParams.get("name");
+  const room = queryParams.get("room");
 
-    useEffect(() => {
+  useEffect(() => {
+    socket.emit("JOIN", { room, name });
 
-        socket.emit("NAME", name)
+    socket.on("JOINUSER", (newUser: message) => {
+      console.log(newUser);
+    });
 
-        socket.on('MESSAGE', (newMessage: message) => {
-            setMessages((prevMessages) => [...prevMessages, newMessage]);
-            console.log(newMessage)
-        });
+    socket.on("MESSAGE", (newMessage: message) => {
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+      console.log(newMessage);
+    });
 
-        setUserId(socket.id || '')
+    setUserId(socket.id || "");
 
-        return () => {
-            socket.off('MESSAGE');
-        };
-    }, []);
-
-    const sendMessage = () => {
-        if (message.trim()) {
-            socket.emit('MESSAGE', message);
-            setUserId(socket.id || '')
-            setMessage('');
-        }
+    return () => {
+      socket.off("MESSAGE");
     };
+  }, []);
 
-    useEffect(() => {
-        const chatContainer = chatContainerRef.current;
-        if (chatContainer != null) {
-            chatContainer.scrollTop = chatContainer.scrollHeight;
-        }
-    }, [messages]);
+  const sendMessage = () => {
+    if (message.trim()) {
+      socket.emit("MESSAGEROOM", message);
+      setUserId(socket.id || "");
+      setMessage("");
+    }
+  };
 
-    return (
-        <>
-            <div className='largo'>
-                <h1>Chat</h1>
-                <div>
-                    <div className='mensagens' ref={chatContainerRef}>
-                        {messages.map((msg: message, index) => {
-                            console.log(msg)
-                            console.log("user: " + userId)
-                            console.log("msg: " + msg.client)
-                            if (userId == msg.client) {
-                                return (
-                                    <div className='mensagem' key={index}>
-                                        <div>                                            
-                                            {msg.text}
-                                        </div>
-                                    </div>
-                                )
-                            } else {
-                                return (
-                                    <div className='mensagem outrouser' style={{ justifyContent: 'flex-start' }} key={index}>
-                                        <div>
-                                            <p className='userName'>{msg.name}</p>
-                                            {msg.text}
-                                        </div>
-                                    </div>
-                                )
-                            }
-                        })}
+  useEffect(() => {
+    const chatContainer = chatContainerRef.current;
+    if (chatContainer != null) {
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+  }, [messages]);
+
+  return (
+    <>
+      <div className="largo">
+        <h1>Chat</h1>
+        <div>
+          <div className="mensagens" ref={chatContainerRef}>
+            {messages.map((msg: message, index) => {
+              console.log(msg);
+              console.log("user: " + msg.name);
+              if (msg.type == "Message") {
+                if (userId == msg.client) {
+                  return (
+                    <div className="mensagem" key={index}>
+                      <div>{msg.text}</div>
                     </div>
-                    <input
-                        type="text"
-                        value={message}
-                        className='inputMessage'
-                        onChange={(e) => setMessage(e.target.value)}
-                        onKeyDown={(e) => {
-                            if (e.key === "Enter") {
-                                sendMessage()
-                            }
-                        }}
-                    />
-                    <button onClick={sendMessage} className='sendButton'><FontAwesomeIcon icon={faPaperPlane} /></button>
-                </div>
-            </div>
-        </>
-    )
+                  );
+                } else {
+                  return (
+                    <div
+                      className="mensagem outrouser"
+                      style={{ justifyContent: "flex-start" }}
+                      key={index}
+                    >
+                      <div>
+                        <p className="userName">{msg.name}</p>
+                        {msg.text}
+                      </div>
+                    </div>
+                  );
+                }
+              } else {
+                return(
+                    <div className="notification">
+                        <p style={{fontSize: 25, color: '#fff',margin: 0,padding: 0}}>{msg.name} entrou</p>
+                    </div>
+                )
+              }
+            })}
+          </div>
+          <input
+            type="text"
+            value={message}
+            className="inputMessage"
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                sendMessage();
+              }
+            }}
+          />
+          <button onClick={sendMessage} className="sendButton">
+            <FontAwesomeIcon icon={faPaperPlane} />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Chat
+export default Chat;
